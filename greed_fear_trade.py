04d9719742d628dee3db7865d9fd5e55a37384f8
Joao Zsigmond <jzsiggy@gmail.com>
2019-08-22 11:29:41 -0300

    Changing algo

--- greed_fear_trade.py
+++ greed_fear_trade.py
@@ -6,14 +6,14 @@ def greed_fear_trade():
     change = yesterday.at[1, "percentChange"]
     btc_balance_in_real = float(get_bitbrasil_balance()["btc"]) * btc_price
     real_balance = float(get_bitbrasil_balance()["brl"])
-    gfi = get_greed_fear_index()
+    gfi, gfi_change = get_greed_fear_index()
 
-    if gfi < 30 and real_balance > 30:
+    if (gfi < 30 or gfi_change < -12) and real_balance > 30:
         r = buy_BRLBTC(real_balance / 2)
         send_msg(r.text)
         send_msg("BUYING BTC")
         send_msg("AMOUNT: {}".format(real_balance / 2))
-    elif gfi > 60 and change > 4 and btc_balance_in_real > 30:
+    elif (gfi > 90 or gfi_change > 15) and btc_balance_in_real > 30:
         r = sell_BRLBTC(btc_balance_in_real / 2)
         send_msg(r.text)
         send_msg("SELLING BTC")
