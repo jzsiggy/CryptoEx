6ba313eaf1119c79efe682a1236d4bd4abd7398d
Joao Zsigmond <jzsiggy@gmail.com>
2019-08-29 15:23:10 -0300

    minor algo change

--- day_trade_BB_rsi.py
+++ day_trade_BB_rsi.py
@@ -12,13 +12,18 @@ def day_trade():
     now = data.loc[len(data)-1]
     # print(now)
 
-    if (now.rsi < 35) and (now.close >= now.lower_band - 12 or now.close <= now.lower_band + 12):
+    if (now.rsi < 35) and (now.close >= now.upper_band - 12):
         print("[ SELL ]")
         paper.sell_btc(now.close, paper.balance_btc)
-    elif (now.rsi > 60) and (now.close >= now.upper_band - 12 or now.close <= now.upper_band + 12):
+
+    elif (now.rsi > 60) and (now.close <= now.lower_band + 12):
         print("[ BUY ]")
         paper.buy_btc(now.close, paper.balance_reais)
+
     print("BALANCE \n [ BTC ] : {0} \n [ REAIS ] : {1}".format(paper.balance_btc, paper.balance_reais))
 
 while True:
     day_trade()
+
+
+# Tamanho da compra depende da distancia da sma
