51ffd0dc0779e4bd259cc9bca1a4c783fddbed14
Joao Zsigmond <jzsiggy@gmail.com>
2019-12-05 14:12:49 -0200

    Adding coins

--- portfolio_create.py
+++ portfolio_create.py
@@ -8,6 +8,8 @@ from model_prediction import get_regression
 
 from sklearn.ensemble import RandomForestRegressor
 
+import sys
+
 def get_coin_list():
   data = requests.get('https://www.binance.com/api/v1/ticker/allPrices')
   all_pairs_prices = json.loads(data.text)
@@ -19,9 +21,33 @@ def plot_all_pairs():
   model = RandomForestRegressor()
   high_acc_pairs = []
 
-  for pair in pairs[0:100]:
+  for pair in pairs:
+    try:
+      print(pair)
+      acc, pred = get_regression(get_binance_close(pair), 3, model)
+      print("The regressor got {0} percent of hits and these are the values of today and tommorrow {1}".format(acc, pred))
+      if acc > 0.65:
+        high_acc_pairs.append(pair)
+    except KeyboardInterrupt:
+      print("The high accuracy pairs until now are:")
+      print(high_acc_pairs)
+      sys.exit(1)
+    except:
+      print("There was an error with pair {}".format(pair))
+      print("The high accuracy pairs until now are:")
+      print(high_acc_pairs)
+  print("The Binance High Accuracy pairs are:")
+  print(high_acc_pairs)
+
+
+def get_values():
+  model = RandomForestRegressor()
+
+  # high_aac_pairs = ['WTCETH', 'STRATBTC', 'SNGLSETH', 'IOTABTC', 'SUBETH', 'SNTBTC', 'XRPETH', 'MODETH', 'RCNBTC', 'BCCETH', 'BATETH', 'XLMBTC', 'RLCETH', 'CHATBTC', 'RPXBTC', 'RPXETH', 'RPXBNB', 'CLOAKETH', 'BCNETH', 'BCNBNB', 'REPBNB', 'TUSDBTC', 'TUSDBNB', 'ZENBTC', 'QKCBTC', 'NPXSETH', 'HOTETH', 'HCETH', 'GOBNB', 'RVNBNB', 'XRPPAX', 'XLMPAX', 'RENBNB', 'XRPUSDC', 'TRXXRP', 'LINKTUSD', 'LINKPAX', 'LINKUSDC', 'BCHABCPAX', 'BCHABCUSDC', 'TRXPAX', 'BNBUSDS', 'BTTTUSD', 'ONGBNB', 'HOTBNB', 'ZILUSDT', 'FETBNB', 'FETBTC', 'XMRBNB', 'DASHBNB', 'DASHUSDT', 'ENJUSDT', 'MITHUSDT', 'ETCUSDC', 'ETCTUSD', 'TFUELTUSD', 'ONEBNB', 'ALGOBNB', 'ALGOBTC', 'GTOUSDT', 'DOGEBNB', 'DOGEBTC', 'DUSKBNB', 'DUSKUSDC', 'ANKRBNB', 'ANKRBTC', 'ANKRUSDT', 'ONTPAX']
+  for pair in high_aac_pairs:
     print(pair)
-    acc, pred = get_regression(get_binance_close(pair), 10, model)
+    acc, pred = get_regression(get_binance_close(pair), 3, model)
     print("The regressor got {0} percent of hits and these are the values of today and tommorrow {1}".format(acc, pred))
 
-plot_all_pairs()
+
+get_coin_list()
